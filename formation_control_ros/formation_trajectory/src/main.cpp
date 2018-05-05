b4c6567ae12089493a60ba02894468ea0d09e2dc
FanmingL <151190065@smail.nju.edu.cn>
2018-05-05 20:05:58 +0800

    fix compiling bug

--- formation_control_ros/formation_trajectory/src/main.cpp
+++ formation_control_ros/formation_trajectory/src/main.cpp
@@ -1,27 +1,25 @@
 /*************************************************************************
-    > File Name: main.cpp
-    > Author: ma6174
-    > Mail: ma6174@163.com 
-    > Created Time: Wed 12 Apr 2017 01:20:34 PM UTC
+ > File Name: main.cpp
+ > Author: ma6174
+ > Mail: ma6174@163.com
+ > Created Time: Wed 12 Apr 2017 01:20:34 PM UTC
  ************************************************************************/
 
 #include<iostream>
 #include"ros/ros.h"
-#include "april_pro/camera_pos.h"
+#include "uav_msgs/uav_position.h"
 #include "std_msgs/Empty.h"
 #include "traj.h"
-#include <fstream>
-
 #define int16_t short
-int debug_flag_ = 1;
+
 ros::Time old1, old2;
-april_pro::camera_pos pos_get1, pos_get2, pos_out1, pos_out2;
+uav_msgs::uav_position pos_get1, pos_get2, pos_out1, pos_out2;
 bool fresh_flag1=false, fresh_flag2=false;
 ros::Publisher path1,path2,alllock,alllandon;
 Target_t target_out1, target_out2;
 Pos_t stable1,stable2;
 std_msgs::Empty empty;
-void position1_handle(const april_pro::camera_pos &msg)
+void position1_handle(const uav_msgs::uav_position &msg)
 {
     float interval = (msg.header.stamp-old1).toSec();
     if (interval>1.0f){
@@ -34,7 +32,7 @@ void position1_handle(const april_pro::camera_pos &msg)
     target_out1 = trajectory_generate1(interval,10*pos_get1.x,10*pos_get1.y,pos_get1.yaw,stable1);
 }
 
-void position2_handle(const april_pro::camera_pos &msg)
+void position2_handle(const uav_msgs::uav_position &msg)
 {
     float interval = (msg.header.stamp-old2).toSec();
     if (interval>1.0f){
@@ -49,58 +47,46 @@ void position2_handle(const april_pro::camera_pos &msg)
 
 
 int main(int argc, char **argv){
-	ros::init(argc, argv, "formation_trajectory");
-	ros::NodeHandle n;
-	old1 = ros::Time::now();
+    ros::init(argc, argv, "formation_trajectory");
+    ros::NodeHandle n;
+    old1 = ros::Time::now();
     old2 = ros::Time::now();
-	path1 = n.advertise<april_pro::camera_pos>("/odroid1/target",1000);
-    path2 = n.advertise<april_pro::camera_pos>("/odroid2/target",1000);
+    path1 = n.advertise<uav_msgs::uav_position>("/odroid1/target",1000);
+    path2 = n.advertise<uav_msgs::uav_position>("/odroid2/target",1000);
     alllock   =    n.advertise<std_msgs::Empty>("/alllock",1);
     alllandon =    n.advertise<std_msgs::Empty>("/alllandon",1);
-	ros::Subscriber sub1 = n.subscribe("/odroid1/position",1,&position1_handle);
-	ros::Subscriber sub2 = n.subscribe("/odroid2/position",1,&position2_handle);
+    ros::Subscriber sub1 = n.subscribe("/odroid1/position",1,&position1_handle);
+    ros::Subscriber sub2 = n.subscribe("/odroid2/position",1,&position2_handle);
     static int tick=0;
     ros::Rate loop_rate(100);
-    std::ofstream outfile;
-    std::string data_path_out = "trajectory_real";
-    data_path_out  += ".csv";
-    outfile.open(data_path_out.c_str());
-    if(!outfile) std::cout<<"error"<<std::endl;
     while (ros::ok())
     {
         ros::spinOnce();
         if (fresh_flag1&&fresh_flag2)
         {
-                       int error_status = getFormationTarget(stable1, stable2, &target_out1, &target_out2);
-             if (debug_flag_)
-	    {
-            outfile<<pos_get1.x<<","<<pos_get1.y<<","
-                   <<pos_get2.x<<","<<pos_get2.y<<","
-                   <<target_out1.x1<<","<<target_out1.y1<<","
-                   <<target_out2.x1<<","<<target_out2.y1<<"\n";
-            } 
-
- 	tick++;
-	if (tick < 20)
-	{
-		if ((pos_get1.x-pos_get2.x)*(pos_get1.x-pos_get2.x) + ((pos_get1.y-pos_get2.y)*(pos_get1.y-pos_get2.y))<(64 + 15) * (64 + 15))
-		{
-			error_status = -1;
-		}
-		else
-		{
-			error_status = 1;
-		}
-		 
-
-	}
-	
-	//	 int    error_status = 1;  
-	  if (error_status == -1)
+            
+            int error_status = getFormationTarget(stable1, stable2, &target_out1, &target_out2);
+            tick++;
+            if (tick < 20)
+            {
+                if ((pos_get1.x-pos_get2.x)*(pos_get1.x-pos_get2.x) + ((pos_get1.y-pos_get2.y)*(pos_get1.y-pos_get2.y))<(64 + 15) * (64 + 15))
+                {
+                    error_status = -1;
+                }
+                else
+                {
+                    error_status = 1;
+                }
+                
+                
+            }
+            
+            //     int    error_status = 1;
+            if (error_status == -1)
             {
                 alllock.publish(empty);
-	//	std::cout<<"postion1 is "<<stable1.x<<",  "<<stable1.y<<std::endl;
-	//	std::cout<<"positon2 is "<<stable2.x<<",  "<<stable2.y<<std::endl;
+                //    std::cout<<"postion1 is "<<stable1.x<<",  "<<stable1.y<<std::endl;
+                //    std::cout<<"positon2 is "<<stable2.x<<",  "<<stable2.y<<std::endl;
                 continue;
             }
             if (error_status == 0)
@@ -108,7 +94,7 @@ int main(int argc, char **argv){
                 alllandon.publish(empty);
                 continue;
             }
-	
+            
             pos_out1.x = target_out1.x1;
             pos_out1.y = target_out1.y1;
             pos_out1.yaw = target_out1.yaw1;
@@ -122,18 +108,17 @@ int main(int argc, char **argv){
             fresh_flag1 = false;
             fresh_flag2 = false;
             //
-		if (target_out1.end_flag&&target_out2.end_flag)
-		{
-			alllandon.publish(empty);
-		}
+            if (target_out1.end_flag&&target_out2.end_flag)
+            {
+                alllandon.publish(empty);
+            }
             
         }
         loop_rate.sleep();
     }
-    outfile.close();
-
-
-	return 0;
+    
+    
+    
+    return 0;
 }
-
 
